4b494dd8c4dfda7eb30ee12df7c279233593173e
Flames Elite <84363801+FlamesLLC@users.noreply.github.com>
2021-11-30 12:42:51 -0800

    SDK to manage VS Code
    v1.0 - bugs fixed and compiled on Nov. 30 12:42 PM FST

---
+++ VSPluginResetFix.sh
@@ -0,0 +1,10 @@
+## this is a file that deletes all the plugins from vs code and resets vs code to default
+rm -rf ~/Library/Application\ Support/Code/User/settings.json
+rm -rf ~/Library/Application\ Support/Code/User/keybindings.json
+rm -rf ~/Library/Application\ Support/Code/User/snippets
+rm -rf ~/Library/Application\ Support/Code/User/extensions
+echo("Thank you for running VSReset 1.0x by Flames Co. Labs")
+echo("You may now exit VS Code")
+
+## reset github git functions on computer
+rm -rf
