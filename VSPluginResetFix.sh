31d81e11ac3ab291f0059a401271d9ef576ef897
Flames Elite <84363801+FlamesLLC@users.noreply.github.com>
2021-12-01 16:50:52 -0800

    Dec 1. 2021 - Compiled Additional changes
    - fixed bugs

--- VSPluginResetFix.sh
+++ VSPluginResetFix.sh
@@ -1,10 +1,16 @@
+echo "Plugin Resetter 0.1a"
 ## this is a file that deletes all the plugins from vs code and resets vs code to default
 rm -rf ~/Library/Application\ Support/Code/User/settings.json
 rm -rf ~/Library/Application\ Support/Code/User/keybindings.json
 rm -rf ~/Library/Application\ Support/Code/User/snippets
 rm -rf ~/Library/Application\ Support/Code/User/extensions
-echo("Thank you for running VSReset 1.0x by Flames Co. Labs")
-echo("You may now exit VS Code")
 
 ## reset github git functions on computer
 rm -rf
+
+## reset the git manager
+rm -rf ~/.gitconfig
+
+## remove get extension creditinals
+rm -rf ~/.git-credentials
+echo "Exiting.."
